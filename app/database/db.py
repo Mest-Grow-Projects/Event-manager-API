9a8d402bed3db486e52df5ecd8369cbddd44488f
george-asiedu <asiedug41@gmail.com>
2025-09-01 23:38:36 +0000

    db connection modified to check for env variable

--- app/database/db.py
+++ app/database/db.py
@@ -2,13 +2,20 @@ from beanie import init_beanie
 from pymongo import AsyncMongoClient
 import os
 from models.user import User
+from ..utils import constants
+
+mongo_client = None
 
 async def init_db():
-    db_url = os.getenv("MONGO_URI")
-    client = AsyncMongoClient(db_url)
+    global mongo_client
+    db_url = os.getenv("DATABASE_URL")
+
+    if not db_url:
+        raise ValueError(constants.error_messages["database_url"])
+
+    mongo_client = AsyncMongoClient(db_url)
 
     await init_beanie(
-        database=client.get_default_database(),
+        database=mongo_client.get_default_database(),
         document_models=[User]
     )
-    print("Database connection initialized...")
