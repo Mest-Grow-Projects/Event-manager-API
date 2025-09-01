03b3580bc4bccd9e76d7172e7ba15494537a1600
george-asiedu <asiedug41@gmail.com>
2025-09-01 22:48:49 +0000

    user model implemented using pydantic and beanie

---
+++ app/database/models/user.py
@@ -0,0 +1,30 @@
+from enum import Enum
+from typing import Annotated
+
+from pydantic import EmailStr, Field
+from datetime import datetime
+from beanie import Document, Indexed
+import pymongo
+
+class Roles(str, Enum):
+    USER = "USER"
+    ORGANIZER = "ORGANIZER"
+    ADMIN = "ADMIN"
+
+class User(Document):
+    name: str
+    email: Annotated[EmailStr, Indexed(unique=True)]
+    phone: str | None
+    location: str | None
+    password: str
+    role: Roles = Roles.USER
+    createdAt: datetime = Field(default_factory=datetime.now())
+    updatedAt: datetime = Field(default_factory=datetime.now())
+
+    class Settings:
+        name = 'users'
+        indexes = [
+            [("email", pymongo.ASCENDING)],
+            [("role", pymongo.ASCENDING)],
+            [("createdAt", pymongo.DESCENDING)],
+        ]
