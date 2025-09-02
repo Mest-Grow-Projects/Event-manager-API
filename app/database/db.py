d1d58d84921f657a58146be124b6878efff07c07
george-asiedu <asiedug41@gmail.com>
2025-09-02 09:39:55 +0000

    ensured the db connection string is loaded from .env

--- app/database/db.py
+++ app/database/db.py
@@ -2,16 +2,18 @@ from beanie import init_beanie
 from pymongo import AsyncMongoClient
 import os
 from app.models.user import User
-from ..core import constants
+from app.core.constants import error_messages
+from dotenv import load_dotenv
 
 mongo_client = None
 
 async def init_db():
     global mongo_client
+    load_dotenv()
     db_url = os.getenv("DATABASE_URL")
 
     if not db_url:
-        raise ValueError(constants.error_messages["database_url"])
+        raise ValueError(error_messages["database_url"])
 
     mongo_client = AsyncMongoClient(db_url)
 
