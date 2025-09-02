b87f0140c7949162a408abdad5a726906c3c8aee
george-asiedu <asiedug41@gmail.com>
2025-09-02 12:19:14 +0000

    implemented jwt authorization using pyjwt

--- app/utils/auth_utils.py
+++ app/utils/auth_utils.py
@@ -1,9 +1,57 @@
+from datetime import timedelta, datetime, timezone
+from typing import Annotated
+from fastapi import Depends, HTTPException, status
 from passlib.context import CryptContext
+import jwt
+from jwt.exceptions import InvalidTokenError
+from app.core.config import get_settings
+from fastapi.security import OAuth2PasswordBearer
+from app.core.constants import status_messages
+from app.schemas.auth import TokenData
+from app.database.repo.user_repo import find_user_by_id
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+SECRET_KEY = get_settings().secret_key
+ALGORITHM = get_settings().algorithm
+access_token_expires = 60
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+def create_login_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=status_messages["credentials_error"],
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+        token_data = TokenData(id=user_id)
+    except InvalidTokenError:
+        raise credentials_exception
+
+    user = await find_user_by_id(int(token_data.id))
+    if user is None:
+        raise credentials_exception
+    return user
