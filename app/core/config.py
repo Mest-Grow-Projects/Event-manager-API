8337fb45c6f3a60f1213105405c4a28f07c92df9
george-asiedu <asiedug41@gmail.com>
2025-09-03 19:38:36 +0000

    token expiration added

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,8 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(
---
+++ app/schemas/auth_schema.py
@@ -0,0 +1,87 @@
+from pydantic import BaseModel, EmailStr, Field, model_validator, field_validator
+import re
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "bearer"
+
+class TokenData(BaseModel):
+    sub: str | None = None
+
+class SignupSchema(BaseModel):
+    name: str = Field(
+        min_length=2,
+        max_length=50,
+        pattern=patterns_regex["name"],
+        description=validations["name"],
+    )
+    email: EmailStr
+    password: str = Field(
+        min_length=8,
+        description=validations["password"],
+    )
+    confirm_password: str = Field(
+        min_length=8,
+        description=validations["password"],
+    )
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        if not re.search(r"[a-z]", value):
+            raise ValueError(validations["lowercase"])
+        if not re.search(r"[A-Z]", value):
+            raise ValueError(validations["uppercase"])
+        if not re.search(r"\d", value):
+            raise ValueError(validations["digit"])
+        if not re.search(r"[@$!%*?&]", value):
+            raise ValueError(validations["special_character"])
+        return value
+
+    @model_validator(mode='after')
+    def check_password_match(self) -> 'SignupSchema':
+        if self.password != self.confirm_password:
+            raise ValueError(validations["password_mismatch"])
+        return self
+
+    model_config = { "extra": "forbid" }
+
+class LoginSchema(BaseModel):
+    email: EmailStr
+    password: str = Field(
+        min_length=8,
+        description=validations["password"],
+    )
+    model_config = {"extra": "forbid"}
+
+class VerifyAccount(BaseModel):
+    code: str = Field(
+        min_length=6,
+        pattern=patterns_regex["code"],
+        description=validations["code"],
+    )
+    model_config = {"extra": "forbid"}
+
+
+class SignupResponse(BaseModel):
+    message: str
+    token: str
+
+
+class VerifyAccountResponse(BaseModel):
+    message: str
+
+class UserResponse(BaseModel):
+    name: str
+    email: str
+    accountStatus: str
+    role: str
+
+class Data(BaseModel):
+    access_token: str
+    user: UserResponse
+
+class LoginResponse(BaseModel):
+    message: str
+    data: Data
