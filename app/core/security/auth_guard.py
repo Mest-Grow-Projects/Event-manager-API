cdbd65ec82ca490e1b5a7fe0aa273b0f0de7b3bc
george-asiedu <asiedug41@gmail.com>
2025-09-05 21:00:15 +0000

    headers removed from exception

--- app/core/security/auth_guard.py
+++ app/core/security/auth_guard.py
@@ -1,20 +1,18 @@
 from typing import Annotated
 from fastapi import Header, HTTPException, status
-from ..config import get_settings
 from ..constants import status_messages
 from app.database.repository.user_repo import find_user_by_id
 from app.schemas.auth_schema import UserResponse
 from app.utils.auth_utils import decode_jwt
 
-settings = get_settings()
+
 AUTH_PREFIX = 'Bearer'
 
 
 async def get_authenticated_user(authorization: Annotated[str | None, Header()] = None) -> UserResponse:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail=status_messages["credentials_error"],
-        headers={"WWW-Authenticate": "Bearer"},
+        detail=status_messages["credentials_error"]
     )
 
     if not authorization:
