7401e6f96b34b70f624458900468dbab6a819862
george-asiedu <asiedug41@gmail.com>
2025-09-05 16:56:50 +0000

    filter and pagination implemented in the get_all_users method

--- app/users/users_service.py
+++ app/users/users_service.py
@@ -1,16 +1,46 @@
+from enum import Enum
+from fastapi import Query
 from app.core.constants import success_messages
 from app.database.repository.user_repo import create_user, get_and_validate_user, validate_updated_data
-from app.database.models.user import User, AccountStatus, Roles
+from app.database.models.user import User, AccountStatus, Roles, Gender
 from app.schemas.auth_schema import SignupSchema
 from app.schemas.users_schema import UpdateUserInfo, ChangeRole
 
 
 class UsersService:
-    async def get_all_users(self):
-        users = await User.find_all().to_list()
+    async def get_all_users(
+        self,
+        name: str | None = None,
+        gender: Gender | None = None,
+        role: Roles | None = None,
+        account_status: AccountStatus | None = None,
+        page: int = Query(1, ge=1),
+        limit: int = Query(10, ge=1, le=100),
+    ):
+        skip = (page - 1) * limit
+        query = {}
+
+        if name:
+            query["name"] = {"$regex": name, "$options": "i"}
+        if gender:
+            query["gender"] = gender.value if isinstance(gender, Enum) else gender
+        if role:
+            query["role"] = role.value if isinstance(role, Enum) else role
+        if account_status:
+            query["accountStatus"] = account_status.value if isinstance(account_status, Enum) else account_status
+
+        total_users_count = await User.find(query).count()
+        users = await User.find(query).skip(skip).limit(limit).to_list()
+
         return {
             "message": success_messages["all_users"],
             "data": users,
+            "pagination": {
+                "total": total_users_count,
+                "page": page,
+                "limit": limit,
+                "total_pages": (total_users_count + limit -1) // limit,
+            }
         }
 
 
