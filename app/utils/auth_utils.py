71fd56663b715c628e9b8d1bf39a4f929e625415
george-asiedu <asiedug41@gmail.com>
2025-09-05 19:34:37 +0000

    get current user function removed

--- app/utils/auth_utils.py
+++ app/utils/auth_utils.py
@@ -1,18 +1,11 @@
 import random
 from datetime import timedelta, datetime, timezone
-from typing import Annotated
-from fastapi import Depends, HTTPException, status
 import jwt
-from jwt.exceptions import InvalidTokenError
 from app.core.config import get_settings
 from fastapi.security import OAuth2PasswordBearer
-from app.core.constants import status_messages
-from app.database.models.user import User
-from app.schemas.auth_schema import TokenData
-from app.database.repository.user_repo import find_user_by_id
+
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
 SECRET_KEY = get_settings().SECRET_KEY
 ALGORITHM = get_settings().ALGORITHM
 access_token_expire = get_settings().ACCESS_TOKEN_EXPIRE_MINUTES
@@ -34,28 +27,6 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None) -> s
 def create_refresh_token(data: dict) -> str:
     expires = timedelta(days=refresh_token_expire)
     return create_access_token(data, expires_delta=expires)
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> User:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail=status_messages["credentials_error"],
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id = payload.get("sub")
-        if user_id is None:
-            raise credentials_exception
-        token_data = TokenData(sub=user_id)
-    except InvalidTokenError:
-        raise credentials_exception
-
-    user = await find_user_by_id(token_data.sub)
-    if user is None:
-        raise credentials_exception
-    return user
 
 
 def generate_verification_code() -> str:
