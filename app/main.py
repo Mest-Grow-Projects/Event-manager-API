ff73306aad6a82da8d7501b892217d2d64c852ff
george-asiedu <asiedug41@gmail.com>
2025-09-05 20:59:26 +0000

    get_authenticated_user function added to users routes as a dependency

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.database.db import init_db
 from app.core.constants import messages, origins
 from app.core.logging_config import logger
+from app.core.security.auth_guard import get_authenticated_user
 
 from app.auth.auth_route import router as auth_router
 from app.users.users_route import router as users_router
@@ -42,4 +43,9 @@ def read_root():
     return messages["welcome"]
 
 app.include_router(auth_router, prefix="/api")
-app.include_router(users_router, prefix="/api")
+
+app.include_router(
+    users_router,
+    prefix="/api",
+    dependencies=[Depends(get_authenticated_user)]
+)
