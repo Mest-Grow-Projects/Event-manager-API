97f4d6c3b71668e52db09a26acb40435fa4f9a15
george-asiedu <asiedug41@gmail.com>
2025-09-05 16:09:19 +0000

    implemented a create_user function to register a new user into the database

--- app/database/repository/user_repo.py
+++ app/database/repository/user_repo.py
@@ -1,6 +1,9 @@
 from fastapi import HTTPException, status
-from app.database.models.user import User
+from app.database.models.user import User, Roles, AccountStatus
 from app.core.constants import status_messages
+from app.schemas.auth_schema import SignupSchema
+from app.utils.auth_utils import get_password_hash
+
 
 async def find_user_by_email(email: str) -> User:
     user = await User.find_one(User.email == email.lower())
@@ -11,6 +14,7 @@ async def find_user_by_email(email: str) -> User:
         )
     return user
 
+
 async def find_user_by_id(user_id: str) -> User:
     user = await User.get(user_id)
     if not user:
@@ -20,6 +24,7 @@ async def find_user_by_id(user_id: str) -> User:
         )
     return user
 
+
 async def check_existing_user(email: str) -> bool:
     existing_user = await User.find_one(User.email == email.lower())
     if existing_user:
@@ -28,3 +33,27 @@ async def check_existing_user(email: str) -> bool:
             detail=status_messages["conflict"]
         )
     return False
+
+
+async def create_user(
+    user: SignupSchema,
+    role: Roles,
+    account_status: AccountStatus = AccountStatus.PENDING
+) -> User:
+    await check_existing_user(str(user.email))
+    hashed_password = get_password_hash(user.password)
+    new_user = User(
+        name=user.name,
+        email=user.email,
+        password=hashed_password,
+        role=role,
+        accountStatus=account_status,
+    )
+    await new_user.insert()
+
+    if not new_user:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=status_messages["failed_user"]
+        )
+    return new_user
