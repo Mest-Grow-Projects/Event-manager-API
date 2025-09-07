94a9a281a1213b3c4bcadb0e9c2e2a7386edca1d
george-asiedu <asiedug41@gmail.com>
2025-09-07 19:07:40 +0000

    timestamp mixin model defined

--- app/database/models/base_mixin.py
+++ app/database/models/base_mixin.py
@@ -2,5 +2,5 @@ from pydantic import BaseModel, Field
 from datetime import datetime, timezone
 
 class TimestampMixin(BaseModel):
-    createdAt: datetime = Field(default_factory=datetime.now(timezone.utc))
-    updatedAt: datetime = Field(default_factory=datetime.now(timezone.utc))
+    createdAt: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    updatedAt: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
