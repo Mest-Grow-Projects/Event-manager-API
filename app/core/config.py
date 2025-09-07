0251a64da994b8bb78bac19fca640a483818af2d
george-asiedu <asiedug41@gmail.com>
2025-09-07 19:06:19 +0000

    env variables names changed

--- app/core/config.py
+++ app/core/config.py
@@ -1,21 +1,18 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
-import os
 
 
 class Settings(BaseSettings):
     ENVIRONMENT: str = 'development'
     DATABASE_URL: str
-    SECRET_KEY: str
-    ALGORITHM: str
+    JWT_SECRET: str
+    JWT_ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
     model_config = SettingsConfigDict(
-        env_file=os.path.join(
-            os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),
-            '.env'
-        ), env_file_encoding='utf-8'
+        env_file='.env',
+        env_file_encoding='utf-8'
     )
 
 
